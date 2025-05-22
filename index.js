57f473086966604cc050ebfe3274f09227e64a89
Tomas Andrada <tandrada1012@icloud.com>
2025-05-22 00:18:01 -0300

    feat: Se mejora manejo de errores

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-function callFakeStoreAPI(method, type, ...args) {
-    const url = `https://fakestoreapi.com/${type}`;
-    switch (method) {
-        case 'GET':
-            fetch(url)
-                .then(response => response.json())
-                .then(data => console.log(data));
-            break;
-        case 'POST':
-            const product = {title: args[0], price: args[1], category: args[2]};
-            fetch('https://fakestoreapi.com/products', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(product)
-            })
-                .then(response => response.json())
-                .then(data => console.log(data));
-            break;
-        case 'DELETE':
-            fetch(url, {
-                method: 'DELETE'
-            })
-                .then(response => response.json())
-                .then(data => console.log(data));
-            break;
-        default:
-            console.log('Invalid method');
+async function callFakeStoreAPI(method, service, ...args) {
+    const url = `https://fakestoreapi.com/${service}`;
+    let response;
+    try {
+        switch (method) {
+            case 'GET':
+                response = await fetch(url);
+                break;
+            case 'POST':
+                const product = {title: args[0], price: args[1], category: args[2]};
+                response = await fetch('https://fakestoreapi.com/products', {
+                    method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(product)
+                });
+                break;
+            case 'DELETE':
+                response = await fetch(url, {
+                    method: 'DELETE'
+                })
+                break;
+            default:
+                console.log('Metodo incorrecto');
+                return
+        }
+        if (!response.ok) {
+            console.log('Error en fetch');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
     }
 }
 
